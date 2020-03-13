dd937a44d4dc807a3bf30fbf6917b340d379d743
Peng-Leii <1130527871@qq.com>
2020-03-13 18:32:13 +0800

    update

---
+++ leetcode/001-100/T23_mergeKLists.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.mergeTwoLists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else lists
+    
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(-1)
+        first = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                dummy.next = l1
+                l1 = l1.next
+                dummy = dummy.next
+            else:
+                dummy.next = l2
+                l2 = l2.next
+                dummy = dummy.next
+        if l1:
+            dummy.next = l1
+        elif l2:
+            dummy.next = l2
+        return first.next
