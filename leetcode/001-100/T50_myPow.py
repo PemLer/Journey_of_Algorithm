6d7929dd6db575a790f9a4d2a3817a5a8b6f9280
Peng-Leii <1130527871@qq.com>
2020-03-21 16:17:18 +0800

    update

--- leetcode/001-100/T50_myPow.py
+++ leetcode/001-100/T50_myPow.py
@@ -1,13 +1,13 @@
 class Solution:
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        if n == 0:
-            return 1
-        if n == -1:
-            return 1/x
-        return self.myPow(x*x,n//2)*([1,x][n%2])
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
 
+        res = 1
+        while n > 0:
+            if n & 1 == 1:
+                res = res * x
+            x = x * x
+            n >>= 1
+        return res
