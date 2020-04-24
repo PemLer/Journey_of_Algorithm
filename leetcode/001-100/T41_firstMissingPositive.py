8446f4c788336943780022f0ff7dc3fd1be73642
Peng-Leii <1130527871@qq.com>
2020-04-24 18:01:49 +0800

    update

--- leetcode/001-100/T41_firstMissingPositive.py
+++ leetcode/001-100/T41_firstMissingPositive.py
@@ -2,23 +2,21 @@ from typing import List
 
 
 class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(n):
-            while nums[i] != i + 1 and nums[i] != nums[nums[i] - 1]:
+            while 0 < nums[i] <= n and nums[i] != nums[nums[i] - 1]:
                 index = nums[i] - 1
                 nums[i], nums[index] = nums[index], nums[i]
 
-        res = []
         for i in range(n):
             if nums[i] != i + 1:
-                res.append(i + 1)
+                return i + 1
 
-        return res
+        return n + 1
 
 
 if __name__ == '__main__':
     sol = Solution()
-    nums = [4, 3, 2, 7, 8, 2, 3, 1]
-    print(sol.findDisappearedNumbers(nums))
-
+    nums = [3, 4, 2, 1]
+    print(sol.firstMissingPositive(nums))
--- leetcode/401-500/T442_findDuplicates.py
+++ leetcode/401-500/T442_findDuplicates.py
@@ -2,23 +2,20 @@ from typing import List
 
 
 class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
         n = len(nums)
         for i in range(n):
             while nums[i] != i + 1 and nums[i] != nums[nums[i] - 1]:
                 index = nums[i] - 1
                 nums[i], nums[index] = nums[index], nums[i]
-
-        res = []
         for i in range(n):
             if nums[i] != i + 1:
-                res.append(i + 1)
-
+                res.append(nums[i])
         return res
 
 
 if __name__ == '__main__':
     sol = Solution()
-    nums = [4, 3, 2, 7, 8, 2, 3, 1]
-    print(sol.findDisappearedNumbers(nums))
-
+    nums = [3, 11, 8, 16, 4, 15, 4, 17, 14, 14, 6, 6, 2, 8, 3, 12, 15, 20, 20, 5]
+    print(sol.findDuplicates(nums))
