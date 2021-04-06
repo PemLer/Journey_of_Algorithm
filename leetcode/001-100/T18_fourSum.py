34a58ae22f4e8f8b3973d34a34822b325289908c
penglei <1130527871@qq.com>
2021-04-06 11:22:24 +0800

    update

--- leetcode/001-100/T18_fourSum.py
+++ leetcode/001-100/T18_fourSum.py
@@ -10,11 +10,15 @@ class Solution:
         nums.sort()
 
         res = []
-        for i in range(n):
+        for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i + 1, n):
-                if j - 1 > i and nums[j] == nums[j - 1]:
+            if nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target:
+                break
+            if nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
                 start, end = j + 1, n - 1
                 while start < end:
